af375789b7cff78753e818636f56f529fabe662b
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-02-24 13:39:56 -0500

    began adding save menu

--- src/menus.cr
+++ src/menus.cr
@@ -1,4 +1,5 @@
 require "crsfml"
+require "json"
 require "../src/textures.cr"
 require "../src/fonts.cr"
 
@@ -57,7 +58,7 @@ module Menus
         when "main_menu"
             SystemMenus.draw_main_menu(window)
         when "save_menu"
-            puts "save menu"
+            SystemMenus.draw_save_menu(window)
         when "settings_menu"
             puts  "settings menu"
         when "hud"
@@ -77,9 +78,19 @@ module Menus
         scale_y = current_size.y.to_f / original_height
 
         MENU_BOX_01.position = SF.vector2(scale_x + 50, scale_y + 300)
+        MENU_BOX_01.size = SF.vector2(150, 80)
         MENU_TEXT_01.position = MENU_BOX_01.position + SF.vector2(35, 15)
+        MENU_TEXT_01.string = "Play                    Settings                    Quit"
+
         MENU_BOX_02.position = MENU_BOX_01.position + SF.vector2(250, 0)
+        MENU_BOX_02.size = SF.vector2(200, 80)
+        MENU_TEXT_02.color = SF::Color::White
+        MENU_TEXT_02.string = "A Normal World"
+        MENU_TEXT_02.character_size = 50
+        MENU_TEXT_02.position = SF.vector2(200, 100)
+
         MENU_BOX_03.position = MENU_BOX_02.position + SF.vector2(300, 0)
+
         window.draw(MENU_BOX_01)
         window.draw(MENU_BOX_02)
         window.draw(MENU_BOX_03)
@@ -112,11 +123,66 @@ module Menus
         
         if (scaled_mouse_x >= menu_box_1_x && scaled_mouse_x <= menu_box_1_x + MENU_BOX_01.size.x) && (scaled_mouse_y >= menu_box_1_y && scaled_mouse_y <= menu_box_1_y + MENU_BOX_01.size.y)
             MENU_BOX_01.outline_color = SF.color(97, 140, 165)
+            this = "save_menu"
+            SystemMenus.system_menu=(this)
         elsif (scaled_mouse_x >= menu_box_2_x && scaled_mouse_x <= menu_box_2_x + 200) && (scaled_mouse_y >= menu_box_2_y && scaled_mouse_y <= menu_box_2_y + 80)
             MENU_BOX_02.outline_color = SF.color(97, 140, 165)
         elsif (scaled_mouse_x >= menu_box_3_x && scaled_mouse_x <= menu_box_3_x + 150) && (scaled_mouse_y >= menu_box_3_y && scaled_mouse_y <= menu_box_3_y + 80)
             MENU_BOX_03.outline_color = SF.color(97, 140, 165)
             window.close
+        end
+     end
+
+     def SystemMenus.draw_save_menu(window)
+        current_size = window.size
+        original_width = 800 
+        original_height = 600
+    
+        scale_x = current_size.x.to_f / original_width
+        scale_y = current_size.y.to_f / original_height
+
+        MENU_BOX_01.position = SF.vector2(scale_x + 20, scale_y + 540)
+        MENU_TEXT_01.position = MENU_BOX_01.position + SF.vector2(15, 1)
+        MENU_BOX_01.size = SF.vector2(115, 40)
+        MENU_TEXT_01.string = "Back"
+
+        MENU_BOX_02.size = SF.vector2(150, 250)
+        MENU_BOX_02.position = SF.vector2(scale_x + 20, scale_y + 20)
+        MENU_TEXT_02.color = SF::Color::Blue
+        MENU_TEXT_02.character_size = 25
+        MENU_TEXT_02.position = MENU_BOX_02.position + SF.vector2(35, 200)
+        if !File.exists?("save01")
+            MENU_TEXT_02.string = "Empty"
+        end
+        window.draw(MENU_BOX_01)
+        window.draw(MENU_BOX_02)
+        window.draw(MENU_TEXT_01)
+        window.draw(MENU_TEXT_02)
+        if SF::Mouse.button_pressed?(SF::Mouse::Left)
+            SystemMenus.save_menu_mouse_handling(window)
+        end
+     end
+
+     def SystemMenus.save_menu_mouse_handling(window)
+        mouse_position = SF::Mouse.get_position(window)
+        mouse_x = mouse_position.x
+        mouse_y = mouse_position.y
+        menu_box_1_x = MENU_BOX_01.position.x
+        menu_box_1_y = MENU_BOX_01.position.y
+        menu_box_2_x = MENU_BOX_02.position.x
+        menu_box_2_y = MENU_BOX_02.position.y
+        current_size = window.size
+        original_width = 800
+        original_height = 600
+    
+        scale_x = current_size.x.to_f / original_width
+        scale_y = current_size.y.to_f / original_height
+    
+        scaled_mouse_x = mouse_x / scale_x
+        scaled_mouse_y = mouse_y / scale_y
+        if (scaled_mouse_x >= menu_box_1_x && scaled_mouse_x <= menu_box_1_x + MENU_BOX_01.size.x) && (scaled_mouse_y >= menu_box_1_y && scaled_mouse_y <= menu_box_1_y + MENU_BOX_01.size.y)
+            this = "main_menu"
+            SystemMenus.system_menu=(this)
         end
      end
 
