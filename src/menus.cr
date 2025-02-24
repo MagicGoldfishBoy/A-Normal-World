d045cb49b620a572fc9275f2b32d8a46b38ce626
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-02-24 12:45:02 -0500

    menu buttons now work when window is  scaled

--- src/menus.cr
+++ src/menus.cr
@@ -69,11 +69,55 @@ module Menus
      end
 
      def SystemMenus.draw_main_menu(window)
+        current_size = window.size
+        original_width = 800 
+        original_height = 600
+    
+        scale_x = current_size.x.to_f / original_width
+        scale_y = current_size.y.to_f / original_height
+
+        MENU_BOX_01.position = SF.vector2(scale_x + 50, scale_y + 300)
+        MENU_TEXT_01.position = MENU_BOX_01.position + SF.vector2(35, 15)
+        MENU_BOX_02.position = MENU_BOX_01.position + SF.vector2(250, 0)
+        MENU_BOX_03.position = MENU_BOX_02.position + SF.vector2(300, 0)
         window.draw(MENU_BOX_01)
         window.draw(MENU_BOX_02)
         window.draw(MENU_BOX_03)
         window.draw(MENU_TEXT_01)
         window.draw(MENU_TEXT_02)
+        if SF::Mouse.button_pressed?(SF::Mouse::Left)
+            SystemMenus.main_menu_mouse_handling(window)
+        end
+     end
+     
+     def SystemMenus.main_menu_mouse_handling(window)
+        mouse_position = SF::Mouse.get_position(window)
+        mouse_x = mouse_position.x
+        mouse_y = mouse_position.y
+        menu_box_1_x = MENU_BOX_01.position.x
+        menu_box_1_y = MENU_BOX_01.position.y
+        menu_box_2_x = MENU_BOX_02.position.x
+        menu_box_2_y = MENU_BOX_02.position.y
+        menu_box_3_x = MENU_BOX_03.position.x
+        menu_box_3_y = MENU_BOX_03.position.y
+        current_size = window.size
+        original_width = 800
+        original_height = 600
+    
+        scale_x = current_size.x.to_f / original_width
+        scale_y = current_size.y.to_f / original_height
+    
+        scaled_mouse_x = mouse_x / scale_x
+        scaled_mouse_y = mouse_y / scale_y
+        
+        if (scaled_mouse_x >= menu_box_1_x && scaled_mouse_x <= menu_box_1_x + MENU_BOX_01.size.x) && (scaled_mouse_y >= menu_box_1_y && scaled_mouse_y <= menu_box_1_y + MENU_BOX_01.size.y)
+            MENU_BOX_01.outline_color = SF.color(97, 140, 165)
+        elsif (scaled_mouse_x >= menu_box_2_x && scaled_mouse_x <= menu_box_2_x + 200) && (scaled_mouse_y >= menu_box_2_y && scaled_mouse_y <= menu_box_2_y + 80)
+            MENU_BOX_02.outline_color = SF.color(97, 140, 165)
+        elsif (scaled_mouse_x >= menu_box_3_x && scaled_mouse_x <= menu_box_3_x + 150) && (scaled_mouse_y >= menu_box_3_y && scaled_mouse_y <= menu_box_3_y + 80)
+            MENU_BOX_03.outline_color = SF.color(97, 140, 165)
+            window.close
+        end
      end
 
     end
