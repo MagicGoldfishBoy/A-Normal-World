62285714e7bb899ab7d14c6071f6ea1a7dec8e68
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-03-16 10:06:53 -0400

    improved shoes mouse handling method

--- src/inventory.cr
+++ src/inventory.cr
@@ -5258,11 +5258,13 @@ module Inventory
         if (mouse_x >= slot_01_x && mouse_x <= slot_01_x + slot_01_width) &&
            (mouse_y >= slot_01_y && mouse_y <= slot_01_y + slot_01_height)
             if @@shoes_slot_01 != nil   
-                t = 0 + (@@page.not_nil! * 15) - 15
+                t = 0 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_01.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_01.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
                 
             end
             ClothingTabShoes.assign_slot_textures(window)
@@ -5273,11 +5275,13 @@ module Inventory
            (mouse_y >= slot_02_y && mouse_y <= slot_02_y + slot_02_height)
            
             if @@shoes_slot_02 != nil
-                t = 1 + (@@page.not_nil! * 15) - 15
+                t = 1 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_02.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_02.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5287,11 +5291,13 @@ module Inventory
            (mouse_y >= slot_03_y && mouse_y <= slot_03_y + slot_03_height)
            
             if @@shoes_slot_03 != nil
-                t = 2 + (@@page.not_nil! * 15) - 15
+                t = 2 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_03.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_03.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5301,11 +5307,13 @@ module Inventory
            (mouse_y >= slot_04_y && mouse_y <= slot_04_y + slot_04_height)
            
             if @@shoes_slot_04 != nil
-                t = 3 + (@@page.not_nil! * 15) - 15
+                t = 3 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_04.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_04.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5315,11 +5323,13 @@ module Inventory
            (mouse_y >= slot_05_y && mouse_y <= slot_05_y + slot_05_height)
            
             if @@shoes_slot_05 != nil
-                t = 4 + (@@page.not_nil! * 15) - 15
+                t = 4 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_05.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_05.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5329,11 +5339,13 @@ module Inventory
            (mouse_y >= slot_06_y && mouse_y <= slot_06_y + slot_06_height)
            
             if @@shoes_slot_06 != nil
-                t = 5 + (@@page.not_nil! * 15) - 15
+                t = 5 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_06.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_06.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5343,11 +5355,13 @@ module Inventory
            (mouse_y >= slot_07_y && mouse_y <= slot_07_y + slot_07_height)
            
             if @@shoes_slot_07 != nil
-                t = 6 + (@@page.not_nil! * 15) - 15
+                t = 6 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_07.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_07.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5357,11 +5371,13 @@ module Inventory
            (mouse_y >= slot_08_y && mouse_y <= slot_08_y + slot_08_height)
            
             if @@shoes_slot_08 != nil
-                t = 7 + (@@page.not_nil! * 15) - 15
+                t = 7 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_08.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_08.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5371,11 +5387,13 @@ module Inventory
            (mouse_y >= slot_09_y && mouse_y <= slot_09_y + slot_09_height)
            
             if @@shoes_slot_09 != nil
-                t = 8 + (@@page.not_nil! * 15) - 15
+                t = 8 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_09.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_09.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5385,11 +5403,13 @@ module Inventory
            (mouse_y >= slot_10_y && mouse_y <= slot_10_y + slot_10_height)
            
             if @@shoes_slot_10 != nil
-                t = 9 + (@@page.not_nil! * 15) - 15
+                t = 9 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_10.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_10.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5399,11 +5419,13 @@ module Inventory
            (mouse_y >= slot_11_y && mouse_y <= slot_11_y + slot_11_height)
            
             if @@shoes_slot_11 != nil
-                t = 10 + (@@page.not_nil! * 15) - 15
+                t = 10 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_11.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_11.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5413,11 +5435,13 @@ module Inventory
            (mouse_y >= slot_12_y && mouse_y <= slot_12_y + slot_12_height)
            
             if @@shoes_slot_12 != nil
-                t = 11 + (@@page.not_nil! * 15) - 15
+                t = 11 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_12.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_12.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5427,11 +5451,13 @@ module Inventory
            (mouse_y >= slot_13_y && mouse_y <= slot_13_y + slot_13_height)
            
             if @@shoes_slot_13 != nil
-                t = 12 + (@@page.not_nil! * 15) - 15
+                t = 12 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_13.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_13.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5441,11 +5467,13 @@ module Inventory
            (mouse_y >= slot_14_y && mouse_y <= slot_14_y + slot_14_height)
            
             if @@shoes_slot_14 != nil
-                t = 13 + (@@page.not_nil! * 15) - 15
+                t = 13 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_14.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_14.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -5455,11 +5483,13 @@ module Inventory
            (mouse_y >= slot_15_y && mouse_y <= slot_15_y + slot_15_height)
            
             if @@shoes_slot_15 != nil
-                t = 14 + (@@page.not_nil! * 15) - 15
+                t = 14 + ((@@page || 1) * 15) - 15
                 
-                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(Player::Appearance.get_clothing("shoes").not_nil!).not_nil!)
-                Player::Appearance.change_shoes(@@shoes_slot_15.not_nil!.name)
+                @@owned_shoes_array[t] = (Clothing::Shoes.get_shoes(
+                Player::Appearance.get_clothing("shoes").try(&.to_s) || Clothing::Shoes.get_shoes("No Shoes").not_nil!)).not_nil!
+                Player::Appearance.change_shoes(@@shoes_slot_15.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_shoes_array.uniq!
             end
             ClothingTabShoes.assign_slot_textures(window)
             sleep 0.15.seconds
