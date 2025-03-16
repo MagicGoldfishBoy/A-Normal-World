5ff971b53905d254687f83a75498720af9ba7127
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-03-16 09:32:59 -0400

    improved pants mouse handling method

--- src/inventory.cr
+++ src/inventory.cr
@@ -3135,7 +3135,7 @@ module Inventory
                 t = 14 + ((@@page || 1) * 15) - 15
                 
                 @@owned_shirt_array[t] = (Clothing::Shirt.get_shirt(
-                    Player::Appearance.get_clothing("shirt").try(&.to_s) || Clothing::Shirt.get_shirt("No Shirt").not_nil!)).not_nil!
+                Player::Appearance.get_clothing("shirt").try(&.to_s) || Clothing::Shirt.get_shirt("No Shirt").not_nil!)).not_nil!
                 Player::Appearance.change_shirt(@@shirt_slot_15.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
                 @@owned_shirt_array.uniq!
@@ -4086,11 +4086,13 @@ module Inventory
         if (mouse_x >= slot_01_x && mouse_x <= slot_01_x + slot_01_width) &&
            (mouse_y >= slot_01_y && mouse_y <= slot_01_y + slot_01_height)
             if @@pants_slot_01 != nil   
-                t = 0 + (@@page.not_nil! * 15) - 15
+                t = 0 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_01.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_01.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4100,11 +4102,13 @@ module Inventory
            (mouse_y >= slot_02_y && mouse_y <= slot_02_y + slot_02_height)
            
             if @@pants_slot_02 != nil
-                t = 1 + (@@page.not_nil! * 15) - 15
+                t = 1 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_02.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_02.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4114,11 +4118,13 @@ module Inventory
            (mouse_y >= slot_03_y && mouse_y <= slot_03_y + slot_03_height)
            
             if @@pants_slot_03 != nil
-                t = 2 + (@@page.not_nil! * 15) - 15
+                t = 2 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_03.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_03.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4128,11 +4134,13 @@ module Inventory
            (mouse_y >= slot_04_y && mouse_y <= slot_04_y + slot_04_height)
            
             if @@pants_slot_04 != nil
-                t = 3 + (@@page.not_nil! * 15) - 15
+                t = 3 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_04.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_04.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4142,11 +4150,13 @@ module Inventory
            (mouse_y >= slot_05_y && mouse_y <= slot_05_y + slot_05_height)
            
             if @@pants_slot_05 != nil
-                t = 4 + (@@page.not_nil! * 15) - 15
+                t = 4 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_05.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_05.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4156,11 +4166,13 @@ module Inventory
            (mouse_y >= slot_06_y && mouse_y <= slot_06_y + slot_06_height)
            
             if @@pants_slot_06 != nil
-                t = 5 + (@@page.not_nil! * 15) - 15
+                t = 5 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_06.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_06.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4170,11 +4182,13 @@ module Inventory
            (mouse_y >= slot_07_y && mouse_y <= slot_07_y + slot_07_height)
            
             if @@pants_slot_07 != nil
-                t = 6 + (@@page.not_nil! * 15) - 15
+                t = 6 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_07.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_07.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4184,11 +4198,13 @@ module Inventory
            (mouse_y >= slot_08_y && mouse_y <= slot_08_y + slot_08_height)
            
             if @@pants_slot_08 != nil
-                t = 7 + (@@page.not_nil! * 15) - 15
+                t = 7 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_08.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_08.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4198,11 +4214,13 @@ module Inventory
            (mouse_y >= slot_09_y && mouse_y <= slot_09_y + slot_09_height)
            
             if @@pants_slot_09 != nil
-                t = 8 + (@@page.not_nil! * 15) - 15
+                t = 8 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_09.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_09.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4212,11 +4230,13 @@ module Inventory
            (mouse_y >= slot_10_y && mouse_y <= slot_10_y + slot_10_height)
            
             if @@pants_slot_10 != nil
-                t = 9 + (@@page.not_nil! * 15) - 15
+                t = 9 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_10.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_10.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4226,11 +4246,13 @@ module Inventory
            (mouse_y >= slot_11_y && mouse_y <= slot_11_y + slot_11_height)
            
             if @@pants_slot_11 != nil
-                t = 10 + (@@page.not_nil! * 15) - 15
+                t = 10 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_11.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_11.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4240,11 +4262,13 @@ module Inventory
            (mouse_y >= slot_12_y && mouse_y <= slot_12_y + slot_12_height)
            
             if @@pants_slot_12 != nil
-                t = 11 + (@@page.not_nil! * 15) - 15
+                t = 11 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_12.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_12.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4254,11 +4278,13 @@ module Inventory
            (mouse_y >= slot_13_y && mouse_y <= slot_13_y + slot_13_height)
            
             if @@pants_slot_13 != nil
-                t = 12 + (@@page.not_nil! * 15) - 15
+                t = 12 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_13.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_13.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4268,11 +4294,13 @@ module Inventory
            (mouse_y >= slot_14_y && mouse_y <= slot_14_y + slot_14_height)
            
             if @@pants_slot_14 != nil
-                t = 13 + (@@page.not_nil! * 15) - 15
+                t = 13 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_14.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_14.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
@@ -4282,11 +4310,13 @@ module Inventory
            (mouse_y >= slot_15_y && mouse_y <= slot_15_y + slot_15_height)
            
             if @@pants_slot_15 != nil
-                t = 14 + (@@page.not_nil! * 15) - 15
+                t = 14 + ((@@page || 1) * 15) - 15
                 
-                @@owned_pants_array[t] = (Clothing::Pants.get_pants(Player::Appearance.get_clothing("pants").not_nil!).not_nil!)
-                Player::Appearance.change_pants(@@pants_slot_15.not_nil!.name)
+                @@owned_pants_array[t] = (Clothing::Pants.get_pants(
+                Player::Appearance.get_clothing("pants").try(&.to_s) || Clothing::Pants.get_pants("No Pants").not_nil!)).not_nil!
+                Player::Appearance.change_pants(@@pants_slot_15.try &.name)
                 Sprites::Player.refresh_player_sprite(window)
+                @@owned_pants_array.uniq!
             end
             ClothingTabPants.assign_slot_textures(window)
             sleep 0.15.seconds
