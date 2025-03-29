a560bd7f7c01e9178c3b8ee12f0fde286508d7b3
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-03-28 21:15:54 -0400

    replaced menu box 3

--- src/ui_elements.cr
+++ src/ui_elements.cr
@@ -41,17 +41,20 @@ require "../src/textures.cr"
         PLAY_BUTTON = MenuBoxes.new("Play Button", "Main1", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 150, 80)), 150.0, 80.0)
         BACK_BUTTON = MenuBoxes.new("Back Button", "Main2", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 115, 40)), 115.0, 40.0)
         SETTINGS_BUTTON = MenuBoxes.new("Settings Button", "Main3", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 200, 80)), 200.0, 80.0)
+        QUIT_BUTTON = MenuBoxes.new("Quit Button", "Main4", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 150, 80)), 150.0, 80.0)
 
         DEBUG_BUTTON = MenuBoxes.new("Debug Button", "Settings1", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 100, 50)), 100.0, 50.0)
 
         LEVEL_EDITOR_BUTTON = MenuBoxes.new("Level Editor Button", "Debug1", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 155, 50)), 155.0, 50.0)
 
         SAVE_BOX_01 = MenuBoxes.new("Save Box 1", "Save01", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 150, 250)), 150.0, 250.0)
+        SAVE_BOX_02 = MenuBoxes.new("Save Box 2", "Save02", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 150, 250)), 150.0, 250.0)
 
         SKIN_BOX_LEFT = MenuBoxes.new("Skin Box Left", "SkinL1", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 115, 40)), 115.0, 40.0)
+        SKIN_BOX_RIGHT = MenuBoxes.new("Skin Box Right", "SkinR1", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 115, 40)), 115.0, 40.0)
 
         MENU_BOX = MenuBoxes.new("Menu Box", "HudMen1", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 120, 40)), 120.0, 40.0)
-        
+
         LEVEL_BOX = MenuBoxes.new("Level Box", "Stats1", SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 115, 40)), 115.0, 40.0)
 
     end
