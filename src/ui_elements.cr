e7aceda229f9f818e417bb3f58e5d74520afd340
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-03-30 10:43:16 -0400

    replaced mune text 2

--- src/ui_elements.cr
+++ src/ui_elements.cr
@@ -21,10 +21,12 @@ require "../src/fonts.cr"
 
     end
     class Ui_TextBase
+        TEXT_ARRAY = [] of Ui_TextBase
         def initialize(name : String, id : String, text : SF::Text)
             @name = name
             @id = id
             @text = text
+            TEXT_ARRAY.push(self)
         end
 
         property name : String
@@ -36,113 +38,110 @@ require "../src/fonts.cr"
     class MenuThemes
         def initialize(
             name : String,
-            texture01 : SF::Texture, color01 : SF::Color,
-            texture02 : SF::Texture, color02 : SF::Color,
-            texture03 : SF::Texture, color03 : SF::Color,
-            texture04 : SF::Texture, color04 : SF::Color
+            menu_texture : SF::Texture, color01 : SF::Color,
+            effect_texture : SF::Texture, color02 : SF::Color,
+            color03 : SF::Color, color04 : SF::Color
             )
 
             @name = name
-            @texture01 = texture01
+            @menu_texture = menu_texture
             @color01 = color01
-            @texture02 = texture02
+            @effect_texture = effect_texture
             @color02 = color02
-            @texture03 = texture03
             @color03 = color03
-            @texture04 = texture04
             @color04 = color04
         end
 
         property name : String
-        property texture01 : SF::Texture
+        property menu_texture : SF::Texture
         property color01 : SF::Color
-        property texture02 : SF::Texture
+        property effect_texture : SF::Texture
         property color02 : SF::Color
-        property texture03 : SF::Texture
         property color03 : SF::Color
-        property texture04 : SF::Texture
         property color04 : SF::Color
 
         class_property current_theme : MenuThemes = PRE_ALPHA_THEME
 
         PRE_ALPHA_THEME = MenuThemes.new(
             "Pre Alpha Theme",
-            BLANK_TEXTURE, SF::Color.new(91, 173, 240),
+            PRE_ALPHA_THEME_TEXTURE, SF::Color.new(91, 173, 240),
             BLANK_TEXTURE, SF::Color.new(52, 152, 235),
-            BLANK_TEXTURE, SF::Color.new(41, 110, 166),
-            PRE_ALPHA_THEME_TEXTURE, SF::Color.new(19, 66, 105)
+            SF::Color.new(41, 110, 166), SF::Color.new(19, 66, 105)
         )
 
         def self.apply_theme
-            MenuBoxes::PLAY_BUTTON.sprite.texture = current_theme.texture04
+            MenuBoxes::PLAY_BUTTON.sprite.texture = current_theme.menu_texture
             MenuBoxes::PLAY_BUTTON.sprite.color = current_theme.color01
 
-            MenuBoxes::BACK_BUTTON.sprite.texture = current_theme.texture04
-            MenuBoxes::BACK_BUTTON.sprite.color = current_theme.color01
+            MenuBoxes::BACK_BUTTON.sprite.texture = current_theme.menu_texture
+            MenuBoxes::BACK_BUTTON.sprite.color = current_theme.color03
 
-            MenuBoxes::SETTINGS_BUTTON.sprite.texture = current_theme.texture04
+            MenuBoxes::SETTINGS_BUTTON.sprite.texture = current_theme.menu_texture
             MenuBoxes::SETTINGS_BUTTON.sprite.color = current_theme.color01
 
-            MenuBoxes::QUIT_BUTTON.sprite.texture = current_theme.texture04
+            MenuBoxes::QUIT_BUTTON.sprite.texture = current_theme.menu_texture
             MenuBoxes::QUIT_BUTTON.sprite.color = current_theme.color01
 
-            MenuBoxes::NEXT_BUTTON.sprite.texture = current_theme.texture04
+            MenuBoxes::NEXT_BUTTON.sprite.texture = current_theme.menu_texture
             MenuBoxes::NEXT_BUTTON.sprite.color = current_theme.color01
 
-            MenuBoxes::DEBUG_BUTTON.sprite.texture = current_theme.texture04
+            MenuBoxes::DEBUG_BUTTON.sprite.texture = current_theme.menu_texture
             MenuBoxes::DEBUG_BUTTON.sprite.color = current_theme.color01
 
-            MenuBoxes::LEVEL_EDITOR_BUTTON.sprite.texture = current_theme.texture04
+            MenuBoxes::LEVEL_EDITOR_BUTTON.sprite.texture = current_theme.menu_texture
             MenuBoxes::LEVEL_EDITOR_BUTTON.sprite.color = current_theme.color01
 
-            MenuBoxes::SAVE_BOX_01.sprite.texture = current_theme.texture04
+            MenuBoxes::SAVE_BOX_01.sprite.texture = current_theme.menu_texture
             MenuBoxes::SAVE_BOX_01.sprite.color = current_theme.color02
-            MenuBoxes::SAVE_BOX_02.sprite.texture = current_theme.texture04
+            MenuBoxes::SAVE_BOX_02.sprite.texture = current_theme.menu_texture
             MenuBoxes::SAVE_BOX_02.sprite.color = current_theme.color02
-            MenuBoxes::SAVE_BOX_03.sprite.texture = current_theme.texture04
+            MenuBoxes::SAVE_BOX_03.sprite.texture = current_theme.menu_texture
             MenuBoxes::SAVE_BOX_03.sprite.color = current_theme.color02
-            MenuBoxes::SAVE_BOX_04.sprite.texture = current_theme.texture04
+            MenuBoxes::SAVE_BOX_04.sprite.texture = current_theme.menu_texture
             MenuBoxes::SAVE_BOX_04.sprite.color = current_theme.color02
-            MenuBoxes::SAVE_BOX_05.sprite.texture = current_theme.texture04
+            MenuBoxes::SAVE_BOX_05.sprite.texture = current_theme.menu_texture
             MenuBoxes::SAVE_BOX_05.sprite.color = current_theme.color02
-            MenuBoxes::SAVE_BOX_06.sprite.texture = current_theme.texture04
+            MenuBoxes::SAVE_BOX_06.sprite.texture = current_theme.menu_texture
             MenuBoxes::SAVE_BOX_06.sprite.color = current_theme.color02
             MenuBoxes::SAVE_BOX_07.sprite.color = current_theme.color02
-            MenuBoxes::SAVE_BOX_07.sprite.texture = current_theme.texture04
+            MenuBoxes::SAVE_BOX_07.sprite.texture = current_theme.menu_texture
 
-            MenuBoxes::SKIN_BOX_LEFT.sprite.texture = current_theme.texture04
+            MenuBoxes::SKIN_BOX_LEFT.sprite.texture = current_theme.menu_texture
             MenuBoxes::SKIN_BOX_LEFT.sprite.color = current_theme.color01
-            MenuBoxes::SKIN_BOX_RIGHT.sprite.texture = current_theme.texture04
+            MenuBoxes::SKIN_BOX_RIGHT.sprite.texture = current_theme.menu_texture
             MenuBoxes::SKIN_BOX_RIGHT.sprite.color = current_theme.color01
-            MenuBoxes::HAIR_BOX_LEFT.sprite.texture = current_theme.texture04
+            MenuBoxes::HAIR_BOX_LEFT.sprite.texture = current_theme.menu_texture
             MenuBoxes::HAIR_BOX_LEFT.sprite.color = current_theme.color01
-            MenuBoxes::HAIR_BOX_RIGHT.sprite.texture = current_theme.texture04
+            MenuBoxes::HAIR_BOX_RIGHT.sprite.texture = current_theme.menu_texture
             MenuBoxes::HAIR_BOX_RIGHT.sprite.color = current_theme.color01
-            MenuBoxes::FACE_BOX_LEFT.sprite.texture = current_theme.texture04
+            MenuBoxes::FACE_BOX_LEFT.sprite.texture = current_theme.menu_texture
             MenuBoxes::FACE_BOX_LEFT.sprite.color = current_theme.color01
-            MenuBoxes::FACE_BOX_RIGHT.sprite.texture = current_theme.texture04
+            MenuBoxes::FACE_BOX_RIGHT.sprite.texture = current_theme.menu_texture
             MenuBoxes::FACE_BOX_RIGHT.sprite.color = current_theme.color01
-            MenuBoxes::SHIRT_BOX_LEFT.sprite.texture = current_theme.texture04
+            MenuBoxes::SHIRT_BOX_LEFT.sprite.texture = current_theme.menu_texture
             MenuBoxes::SHIRT_BOX_LEFT.sprite.color = current_theme.color01
-            MenuBoxes::SHIRT_BOX_RIGHT.sprite.texture = current_theme.texture04
+            MenuBoxes::SHIRT_BOX_RIGHT.sprite.texture = current_theme.menu_texture
             MenuBoxes::SHIRT_BOX_RIGHT.sprite.color = current_theme.color01
-            MenuBoxes::PANTS_BOX_LEFT.sprite.texture = current_theme.texture04
+            MenuBoxes::PANTS_BOX_LEFT.sprite.texture = current_theme.menu_texture
             MenuBoxes::PANTS_BOX_LEFT.sprite.color = current_theme.color01
-            MenuBoxes::PANTS_BOX_RIGHT.sprite.texture = current_theme.texture04
+            MenuBoxes::PANTS_BOX_RIGHT.sprite.texture = current_theme.menu_texture
             MenuBoxes::PANTS_BOX_RIGHT.sprite.color = current_theme.color01
-            MenuBoxes::SHOES_BOX_LEFT.sprite.texture = current_theme.texture04
+            MenuBoxes::SHOES_BOX_LEFT.sprite.texture = current_theme.menu_texture
             MenuBoxes::SHOES_BOX_LEFT.sprite.color = current_theme.color01
-            MenuBoxes::SHOES_BOX_RIGHT.sprite.texture = current_theme.texture04
+            MenuBoxes::SHOES_BOX_RIGHT.sprite.texture = current_theme.menu_texture
             MenuBoxes::SHOES_BOX_RIGHT.sprite.color = current_theme.color01
 
-            MenuBoxes::MENU_BOX.sprite.texture = current_theme.texture04
+            MenuBoxes::MENU_BOX.sprite.texture = current_theme.menu_texture
             MenuBoxes::MENU_BOX.sprite.color = current_theme.color01
 
-            MenuBoxes::MAIN_HUD_BOX.sprite.texture = current_theme.texture04
+            MenuBoxes::MAIN_HUD_BOX.sprite.texture = current_theme.menu_texture
             MenuBoxes::MAIN_HUD_BOX.sprite.color = current_theme.color04
 
-            MenuBoxes::LEVEL_BOX.sprite.texture = current_theme.texture04
+            MenuBoxes::LEVEL_BOX.sprite.texture = current_theme.menu_texture
             MenuBoxes::LEVEL_BOX.sprite.color = current_theme.color01
+            MenuText::TEXT_ARRAY.each do |text|
+                text.text.color = current_theme.color04
+            end
         end
 
     end
@@ -189,21 +188,27 @@ require "../src/fonts.cr"
     class MenuText < Ui_TextBase
         PLAY_BUTTON_TEXT = MenuText.new("Play Button Text", "Main1", SF::Text.new("Play                    Settings                    Quit", QUICKSAND, 34))
 
-        BACK_BUTTON_TEXT = MenuText.new("Back Button Text", "Main2", SF::Text.new("Back", QUICKSAND, 34))
-        
-        SETTINGS_BUTTON_TEXT = MenuText.new("Settings Button Text", "Main3", SF::Text.new("Settings", QUICKSAND, 34))
-        QUIT_BUTTON_TEXT = MenuText.new("Quit Button Text", "Main4", SF::Text.new("Quit", QUICKSAND, 34))
+        BACK_BUTTON_TEXT = MenuText.new("Back Button Text", "Main2", SF::Text.new("Back", QUICKSAND, 28))
+
+        TITLE_TEXT = MenuText.new("Settings Button Text", "Main3", SF::Text.new("A Normal World", QUICKSAND, 50))
+
         NEXT_BUTTON_TEXT = MenuText.new("Next Button Text", "Main5", SF::Text.new("Next", QUICKSAND, 34))
-        DEBUG_BUTTON_TEXT = MenuText.new("Debug Button Text", "Settings1", SF::Text.new("Debug", QUICKSAND, 34))
-        LEVEL_EDITOR_BUTTON_TEXT = MenuText.new("Level Editor Button Text", "Debug1", SF::Text.new("Level Editor", QUICKSAND, 34))
-        SAVE_BOX_01_TEXT = MenuText.new("Save Box 1 Text", "Save01", SF::Text.new("Save 1", QUICKSAND, 34))
-        SAVE_BOX_02_TEXT = MenuText.new("Save Box 2 Text", "Save02", SF::Text.new("Save 2", QUICKSAND, 34))
-        SAVE_BOX_03_TEXT = MenuText.new("Save Box 3 Text", "Save03", SF::Text.new("Save 3", QUICKSAND, 34))
-        SAVE_BOX_04_TEXT = MenuText.new("Save Box 4 Text", "Save04", SF::Text.new("Save 4", QUICKSAND, 34))
-        SAVE_BOX_05_TEXT = MenuText.new("Save Box 5 Text", "Save05", SF::Text.new("Save 5", QUICKSAND, 34))
-        SAVE_BOX_06_TEXT = MenuText.new("Save Box 6 Text", "Save06", SF::Text.new("Save 6", QUICKSAND, 34))
-        SAVE_BOX_07_TEXT = MenuText.new("Save Box 7 Text", "Save07", SF::Text.new("Save 7", QUICKSAND, 34))
-        SKIN_BOX_LEFT_TEXT = MenuText.new("Skin Box Left Text", "SkinL1", SF::Text.new("Skin Left", QUICKSAND, 34))
+
+        DEBUG_BUTTON_TEXT = MenuText.new("Debug Button Text", "Settings1", SF::Text.new("Debug", QUICKSAND, 25))
+        
+        LEVEL_EDITOR_BUTTON_TEXT = MenuText.new("Level Editor Button Text", "Debug1", SF::Text.new("Level Editor", QUICKSAND, 25))
+ 
+
+        SAVE_BOX_01_TEXT = MenuText.new("Save Box 1 Text", "Save01", SF::Text.new("Save 1", QUICKSAND, 25))
+
+        SAVE_BOX_02_TEXT = MenuText.new("Save Box 2 Text", "Save02", SF::Text.new("Save 2", QUICKSAND, 25))
+        SAVE_BOX_03_TEXT = MenuText.new("Save Box 3 Text", "Save03", SF::Text.new("Save 3", QUICKSAND, 25))
+        SAVE_BOX_04_TEXT = MenuText.new("Save Box 4 Text", "Save04", SF::Text.new("Save 4", QUICKSAND, 25))
+        SAVE_BOX_05_TEXT = MenuText.new("Save Box 5 Text", "Save05", SF::Text.new("Save 5", QUICKSAND, 25))
+        SAVE_BOX_06_TEXT = MenuText.new("Save Box 6 Text", "Save06", SF::Text.new("Save 6", QUICKSAND, 25))
+        SAVE_BOX_07_TEXT = MenuText.new("Save Box 7 Text", "Save07", SF::Text.new("Save 7", QUICKSAND, 25))
+
+        SKIN_BOX_LEFT_TEXT = MenuText.new("Skin Box Left Text", "SkinL1", SF::Text.new("Skin Left", QUICKSAND, 24))
         SKIN_BOX_RIGHT_TEXT = MenuText.new("Skin Box Right Text", "SkinR1", SF::Text.new("Skin Right", QUICKSAND, 34))
         HAIR_BOX_LEFT_TEXT = MenuText.new("Hair Box Left Text", "HairL1", SF::Text.new("Hair Left", QUICKSAND, 34))
         HAIR_BOX_RIGHT_TEXT = MenuText.new("Hair Box Right Text", "HairR1", SF::Text.new("Hair Right", QUICKSAND, 34))
@@ -215,7 +220,8 @@ require "../src/fonts.cr"
         PANTS_BOX_RIGHT_TEXT = MenuText.new("Pants Box Right Text", "PantsR1", SF::Text.new("Pants Right", QUICKSAND, 34))
         SHOES_BOX_LEFT_TEXT = MenuText.new("Shoes Box Left Text", "ShoesL1", SF::Text.new("Shoes Left", QUICKSAND, 34))
         SHOES_BOX_RIGHT_TEXT = MenuText.new("Shoes Box Right Text", "ShoesR1", SF::Text.new("Shoes Right", QUICKSAND, 34))
-        MENU_BOX_TEXT = MenuText.new("Menu Box Text", "HudMen1", SF::Text.new("Menu", QUICKSAND, 34))
+
+        MENU_BOX_TEXT = MenuText.new("Menu Box Text", "HudMen1", SF::Text.new("Menu", QUICKSAND, 24))
         MAIN_HUD_BOX_TEXT = MenuText.new("Main Hud Box Text", "HUD1", SF::Text.new("Main Hud", QUICKSAND, 34))
         LEVEL_BOX_TEXT = MenuText.new("Level Box Text", "Stats1", SF::Text.new("Level", QUICKSAND, 34))
     end
