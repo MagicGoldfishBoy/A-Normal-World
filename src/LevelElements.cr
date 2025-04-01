ba131f389fe2eaa4f6a5604f41f8df0fb6dd6217
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-04-01 09:40:27 -0400

    began level editor

---
+++ src/LevelElements.cr
@@ -0,0 +1,21 @@
+require "crsfml"
+require "../src/textures.cr"
+
+module LevelElements
+    class PlatformBase
+    end
+    class WallBase
+    end
+    class ClimbeableBase
+    end
+    class TeleportBase
+    end
+    class FluidBase
+    end
+    class DecorBase
+    end
+    class EnemyBase
+    end
+    class SpikeBase
+    end
+end
---
+++ src/level_editor.cr
@@ -0,0 +1,8 @@
+require "crsfml"
+require "../src/textures.cr"
+
+module LevelEditor
+    class LevelDisplay
+        
+    end
+end
