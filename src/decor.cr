b4f258add32ae9f54f9ebfe83e6834181edba53d
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-04-03 19:46:53 -0400

    can spawn decor

--- src/decor.cr
+++ src/decor.cr
@@ -7,6 +7,6 @@ module Decor include LevelElements
         @@fallen_leaves = DecorPlants.new("Fallen Leaves", "fallen_leaves", 0, 0, 
         SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 10, 20)), 1)
         DECOR_SPRITE_HASH["fallen_leaves"] = SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(0, 0, 10, 20))
-        LevelElements::PlatformBase::DECOR_TEMPLATE_ARRAY << @@fallen_leaves
+        LevelElements::DecorBase::DECOR_TEMPLATE_ARRAY << @@fallen_leaves
     end
 end
