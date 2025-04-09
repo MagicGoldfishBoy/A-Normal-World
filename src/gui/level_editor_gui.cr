38fc88f32c2e5ceaedcf6245b2923138935296fa
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-04-08 21:54:49 -0400

    refactored level editor

--- src/gui/level_editor_gui.cr
+++ src/gui/level_editor_gui.cr
@@ -29,15 +29,15 @@ module LevelEditorGui
         CURRENT_ELEMENT_BOX_TEXT = Ui_Elements::MenuText.new("Current Element Text", "LevEdMen2", 
         SF::Text.new("", QUICKSAND, 12))
 
-        CURRENT_LEFT_ARROW = Ui_Elements::MenuBoxes.new("Selected Level Left Arrow", "LevEdMen6", 
+        CURRENT_ELEMENT_LEFT_ARROW = Ui_Elements::MenuBoxes.new("Selected Level Left Arrow", "LevEdMen6", 
         SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(900, 50, 50, 40)), 50.0, 40.0, 2)
 
-        CURRENT_RIGHT_ARROW = Ui_Elements::MenuBoxes.new("Selected Level Right Arrow", "LevEdMen7", 
+        CURRENT_ELEMENT_RIGHT_ARROW = Ui_Elements::MenuBoxes.new("Selected Level Right Arrow", "LevEdMen7", 
         SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(950, 50, 50, 40)), 50.0, 40.0, 2)
 
 
 
-        CURRENT_TEMPLATE_CATEGORY_MENU = Ui_Elements::MenuBoxes.new("Level Element Type Menu", "LevEdMen7", 
+        CURRENT_TEMPLATE_CATEGORY_BOX = Ui_Elements::MenuBoxes.new("Level Element Type Menu", "LevEdMen7", 
         SF::Sprite.new(BLANK_TEXTURE, SF::Rect.new(900, 0, 160, 40)), 160.0, 40.0, 1)
         CURRENT_TEMPLATE_CATEGORY_TEXT = Ui_Elements::MenuText.new("Level Element Type Text", "LevEdMen3", 
         SF::Text.new("", QUICKSAND, 24))
@@ -89,30 +89,28 @@ module LevelEditorGui
                 LevelEditorGuiElements::CURRENT_ELEMENT_BOX_TEXT.text.position = LevelEditorGuiElements::CURRENT_ELEMENT_BOX.sprite.position + SF.vector2(75 * clamped_scale, 12 * scale_ratio)
                 LevelEditorGuiElements::CURRENT_ELEMENT_BOX_TEXT.text.string = LevelEditor::LevelDisplay.current_element.name
     
-                # LevelEditorGuiElements::SELECTED_LEVEL_LEFT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
-                # LevelEditorGuiElements::SELECTED_LEVEL_LEFT_ARROW.sprite.position = LevelEditorGuiElements::SELECTED_LEVEL_ELEMENT_BOX.sprite.position - SF.vector2(50 * scale_x, 0)
-                # LevelEditorGuiElements::SELECTED_LEVEL_RIGHT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
+                LevelEditorGuiElements::CURRENT_ELEMENT_LEFT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
+                LevelEditorGuiElements::CURRENT_ELEMENT_LEFT_ARROW.sprite.position = LevelEditorGuiElements::CURRENT_ELEMENT_BOX.sprite.position - SF.vector2(50 * scale_x, 0)
     
-                # LevelEditorGuiElements::SELECTED_LEVEL_RIGHT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
-                # LevelEditorGuiElements::SELECTED_LEVEL_RIGHT_ARROW.sprite.position = LevelEditorGuiElements::SELECTED_LEVEL_ELEMENT_BOX.sprite.position +
-                # SF.vector2(LevelEditorGuiElements::SELECTED_LEVEL_ELEMENT_BOX.sprite.global_bounds.width, 0)
+                LevelEditorGuiElements::CURRENT_ELEMENT_RIGHT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
+                LevelEditorGuiElements::CURRENT_ELEMENT_RIGHT_ARROW.sprite.position = LevelEditorGuiElements::CURRENT_ELEMENT_BOX.sprite.position +
+                SF.vector2(LevelEditorGuiElements::CURRENT_ELEMENT_BOX.sprite.global_bounds.width, 0)
     
     
-                # LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_MENU.sprite.scale = SF.vector2(scale_x, scale_y)
-                # LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_MENU.sprite.position = LevelEditorGuiElements::MAIN_LEVEL_EDITOR_BOX.sprite.position + SF.vector2(325 * scale_x, 55 * scale_y)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_BOX.sprite.scale = SF.vector2(scale_x, scale_y)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_BOX.sprite.position = LevelEditorGuiElements::MAIN_LEVEL_EDITOR_BOX.sprite.position + SF.vector2(325 * scale_x, 55 * scale_y)
     
-                # LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_TEXT.text.scale = SF.vector2(scale_x, scale_y)
-                # LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_TEXT.text.position = LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_MENU.sprite.position + SF.vector2(75 * clamped_scale, 12 * scale_ratio)
-                # LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_TEXT.text.string = LevelEditor::LevelEditorLogic.current_element_type
-                # Utility::StringUtilities.center_text(LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_TEXT.text)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_TEXT.text.scale = SF.vector2(scale_x, scale_y)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_TEXT.text.position = LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_BOX.sprite.position + SF.vector2(75 * clamped_scale, 12 * scale_ratio)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_TEXT.text.string = LevelEditor::LevelEditorLogic.current_element_type
+                Utility::StringUtilities.center_text(LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_TEXT.text)
     
-                # LevelEditorGuiElements::LEVEL_ELEMENT_LEFT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
-                # LevelEditorGuiElements::LEVEL_ELEMENT_LEFT_ARROW.sprite.position = LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_MENU.sprite.position - SF.vector2(50 * scale_x, 0)
-                # LevelEditorGuiElements::LEVEL_ELEMENT_LEFT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_LEFT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_LEFT_ARROW.sprite.position = LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_BOX.sprite.position - SF.vector2(50 * scale_x, 0)
     
-                # LevelEditorGuiElements::LEVEL_ELEMENT_RIGHT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
-                # LevelEditorGuiElements::LEVEL_ELEMENT_RIGHT_ARROW.sprite.position = LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_MENU.sprite.position +
-                # SF.vector2(LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_MENU.sprite.global_bounds.width, 0)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_RIGHT_ARROW.sprite.scale = SF.vector2(scale_x, scale_y)
+                LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_RIGHT_ARROW.sprite.position = LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_BOX.sprite.position +
+                SF.vector2(LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_BOX.sprite.global_bounds.width, 0)
             end
             LevelEditor::LevelDisplay.draw_level(window)
             window_size = window.size
@@ -132,13 +130,13 @@ module LevelEditorGui
     
             window.draw(LevelEditorGuiElements::CURRENT_ELEMENT_BOX.sprite)
             window.draw(LevelEditorGuiElements::CURRENT_ELEMENT_BOX_TEXT.text)
-            # window.draw(LevelEditorGuiElements::SELECTED_LEVEL_LEFT_ARROW.sprite)
-            # window.draw(LevelEditorGuiElements::SELECTED_LEVEL_RIGHT_ARROW.sprite)
+            window.draw(LevelEditorGuiElements::CURRENT_ELEMENT_LEFT_ARROW.sprite)
+            window.draw(LevelEditorGuiElements::CURRENT_ELEMENT_RIGHT_ARROW.sprite)
     
-            # window.draw(LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_MENU.sprite)
-            # window.draw(LevelEditorGuiElements::LEVEL_ELEMENT_TYPE_TEXT.text)
-            # window.draw(LevelEditorGuiElements::LEVEL_ELEMENT_LEFT_ARROW.sprite)
-            # window.draw(LevelEditorGuiElements::LEVEL_ELEMENT_RIGHT_ARROW.sprite)
+            window.draw(LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_BOX.sprite)
+            window.draw(LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_TEXT.text)
+            window.draw(LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_LEFT_ARROW.sprite)
+            window.draw(LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_RIGHT_ARROW.sprite)
             if SF::Mouse.button_pressed?(SF::Mouse::Left)
                 LevelEditorGuiMouseHandling.level_editor_ui_mouse_handling(window)
             end
@@ -183,44 +181,39 @@ module LevelEditorGui
                 LevelEditor::LevelEditorLogic.update_spawned_element_array
 
                 sleep 0.15.seconds
+            elsif MouseHandling::ClickHandling.button_clicked?(LevelEditorGuiElements::CURRENT_ELEMENT_LEFT_ARROW.sprite, scaled_mouse_x, scaled_mouse_y)
+                LevelEditor::LevelEditorLogic.spawned_element_index = Utility::ArrayUtilities.navigate_array("backward", LevelEditor::LevelEditorLogic.spawned_element_array, 
+                LevelEditor::LevelEditorLogic.spawned_element_index)
+    
+                if LevelEditor::LevelEditorLogic.spawned_element_array.size > 0
+                    LevelEditor::LevelDisplay.current_element = LevelEditor::LevelEditorLogic.spawned_element_array[LevelEditor::LevelEditorLogic.spawned_element_index]
+                end
+
+              sleep 0.15.seconds
+            elsif MouseHandling::ClickHandling.button_clicked?(LevelEditorGuiElements::CURRENT_ELEMENT_RIGHT_ARROW.sprite, scaled_mouse_x, scaled_mouse_y)
+                LevelEditor::LevelEditorLogic.spawned_element_index = Utility::ArrayUtilities.navigate_array("forward", LevelEditor::LevelEditorLogic.spawned_element_array, 
+                LevelEditor::LevelEditorLogic.spawned_element_index)
+    
+                if LevelEditor::LevelEditorLogic.spawned_element_array.size > 0
+                    LevelEditor::LevelDisplay.current_element = LevelEditor::LevelEditorLogic.spawned_element_array[LevelEditor::LevelEditorLogic.spawned_element_index]
+                end
+
+              sleep 0.15.seconds
+            elsif MouseHandling::ClickHandling.button_clicked?(LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_LEFT_ARROW.sprite, scaled_mouse_x, scaled_mouse_y)
+                LevelEditor::LevelEditorLogic.current_element_index = Utility::ArrayUtilities.navigate_array("backward", LevelEditor::LevelEditorLogic.element_array, LevelEditor::LevelEditorLogic.current_element_index)
+                LevelEditor::LevelEditorLogic.current_element_type = LevelEditor::LevelEditorLogic.element_array[LevelEditor::LevelEditorLogic.current_element_index]
+                LevelEditor::LevelEditorLogic.set_current_index
+                LevelEditor::LevelEditorLogic.set_current_array
+
+                sleep 0.15.seconds
+            elsif MouseHandling::ClickHandling.button_clicked?(LevelEditorGuiElements::CURRENT_TEMPLATE_CATEGORY_RIGHT_ARROW.sprite, scaled_mouse_x, scaled_mouse_y)
+                LevelEditor::LevelEditorLogic.current_element_index = Utility::ArrayUtilities.navigate_array("forward", LevelEditor::LevelEditorLogic.element_array, LevelEditor::LevelEditorLogic.current_element_index)
+                LevelEditor::LevelEditorLogic.current_element_type = LevelEditor::LevelEditorLogic.element_array[LevelEditor::LevelEditorLogic.current_element_index]
+                LevelEditor::LevelEditorLogic.set_current_index
+                LevelEditor::LevelEditorLogic.set_current_array
+
+                sleep 0.15.seconds
             end
-            # if (scaled_mouse_x >= menu_box_5_x / scale_x && scaled_mouse_x <= menu_box_5_x + Ui_Elements::MenuBoxes::SELECTED_LEVEL_LEFT_ARROW.width / scale_x) && 
-            #     (scaled_mouse_y >= menu_box_5_y / scale_y && scaled_mouse_y <= menu_box_5_y / scale_y + Ui_Elements::MenuBoxes::SELECTED_LEVEL_LEFT_ARROW.height / scale_y)
-    
-            #     LevelEditor::LevelEditorLogic.spawned_element_index = Utility::ArrayUtilities.navigate_array("backward", LevelEditor::LevelEditorLogic.spawned_element_array, 
-            #     LevelEditor::LevelEditorLogic.spawned_element_index)
-    
-            #     if LevelEditor::LevelEditorLogic.spawned_element_array.size > 0
-            #         LevelEditor::LevelDisplay.current_element = LevelEditor::LevelEditorLogic.spawned_element_array[LevelEditor::LevelEditorLogic.spawned_element_index]
-            #     end
-            #   sleep 0.15.seconds
-            # end
-            # if (scaled_mouse_x >= menu_box_6_x / scale_x && scaled_mouse_x <= menu_box_6_x + Ui_Elements::MenuBoxes::SELECTED_LEVEL_RIGHT_ARROW.width / scale_x) && 
-            #     (scaled_mouse_y >= menu_box_6_y / scale_y && scaled_mouse_y <= menu_box_6_y / scale_y + Ui_Elements::MenuBoxes::SELECTED_LEVEL_RIGHT_ARROW.height / scale_y)
-            #     LevelEditor::LevelEditorLogic.spawned_element_index = Utility::ArrayUtilities.navigate_array("forward", LevelEditor::LevelEditorLogic.spawned_element_array, 
-            #     LevelEditor::LevelEditorLogic.spawned_element_index)
-    
-            #     if LevelEditor::LevelEditorLogic.spawned_element_array.size > 0
-            #         LevelEditor::LevelDisplay.current_element = LevelEditor::LevelEditorLogic.spawned_element_array[LevelEditor::LevelEditorLogic.spawned_element_index]
-            #     end
-            #   sleep 0.15.seconds
-            # end
-            # if (scaled_mouse_x >= menu_box_7_x / scale_x && scaled_mouse_x <= menu_box_7_x + Ui_Elements::MenuBoxes::SELECTED_LEVEL_RIGHT_ARROW.width / scale_x) && 
-            #     (scaled_mouse_y >= menu_box_7_y / scale_y && scaled_mouse_y <= menu_box_7_y / scale_y + Ui_Elements::MenuBoxes::SELECTED_LEVEL_RIGHT_ARROW.height / scale_y)
-            #     LevelEditor::LevelEditorLogic.current_element_index = Utility::ArrayUtilities.navigate_array("backward", LevelEditor::LevelEditorLogic.element_array, LevelEditor::LevelEditorLogic.current_element_index)
-            #     LevelEditor::LevelEditorLogic.current_element_type = LevelEditor::LevelEditorLogic.element_array[LevelEditor::LevelEditorLogic.current_element_index]
-            #     LevelEditor::LevelEditorLogic.set_current_index
-            #     LevelEditor::LevelEditorLogic.set_current_array
-            #     sleep 0.15.seconds
-            # end
-            # if (scaled_mouse_x >= menu_box_8_x / scale_x && scaled_mouse_x <= menu_box_8_x + Ui_Elements::MenuBoxes::SELECTED_LEVEL_RIGHT_ARROW.width / scale_x) && 
-            #     (scaled_mouse_y >= menu_box_8_y / scale_y && scaled_mouse_y <= menu_box_8_y / scale_y + Ui_Elements::MenuBoxes::SELECTED_LEVEL_RIGHT_ARROW.height / scale_y)
-            #     LevelEditor::LevelEditorLogic.current_element_index = Utility::ArrayUtilities.navigate_array("forward", LevelEditor::LevelEditorLogic.element_array, LevelEditor::LevelEditorLogic.current_element_index)
-            #     LevelEditor::LevelEditorLogic.current_element_type = LevelEditor::LevelEditorLogic.element_array[LevelEditor::LevelEditorLogic.current_element_index]
-            #     LevelEditor::LevelEditorLogic.set_current_index
-            #     LevelEditor::LevelEditorLogic.set_current_array
-            #     sleep 0.15.seconds
-            # end
          end
     end
 end
