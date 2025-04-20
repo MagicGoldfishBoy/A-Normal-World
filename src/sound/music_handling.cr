aca3278b2b2a2c0f357386163780f7b9873f5783
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-04-20 15:07:21 -0400

    added chiton dump ambiance

--- src/sound/music_handling.cr
+++ src/sound/music_handling.cr
@@ -17,6 +17,7 @@ module MusicHandling
 
         def self.play_menu_music(music)
          self.stop_all_menu_music
+         MapMusic.stop_all_map_music
           case music
            when "main_menu"
             MAIN_THEME.volume = MenuMusic.volume
@@ -28,5 +29,24 @@ module MusicHandling
         end
     end
     class MapMusic
+        def initialize(volume : Int32)
+            @volume = volume
+        end
+
+        class_property volume : Int32 = 100
+
+        def self.stop_all_map_music
+            CHITON_CITY_DUMP_THEME.stop
+        end
+
+        def self.play_map_music(music)
+            MenuMusic.stop_all_menu_music
+            self.stop_all_map_music
+            case music
+            when "chitòn_dump"
+                CHITON_CITY_DUMP_THEME.volume = MapMusic.volume
+                CHITON_CITY_DUMP_THEME.play
+            end
+        end
     end
 end
