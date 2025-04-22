10a555f234e5b707b30ce80780182c9a55eb2643
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-04-22 16:33:52 -0400

    can climb climbeables

--- src/locations/maps.cr
+++ src/locations/maps.cr
@@ -10,6 +10,7 @@ module Maps
             @region = region
             @recommended_lvl = recommended_lvl
             @filename = filename
+            @gravity = gravity
         end
 
         property name : String
@@ -18,6 +19,8 @@ module Maps
         property region : String
         property recommended_lvl : Int32
         property filename : String
+
+        class_property gravity : Float64 = 1
 
         class_property level_platform_array : Array(LevelElements::PlatformBase) = [] of LevelElements::PlatformBase
 
@@ -48,26 +51,49 @@ module Maps
         end
 
 
-        def self.gravitational_pull(entity, gravity)
-            if self.gravity_clock.elapsed_time > SF.seconds(1) 
-            entity.move(SF.vector2(0, gravity))
+        def self.gravitational_pull(entity, gravity, window)
+            if self.gravity_clock.elapsed_time > SF.seconds(1)
+                if  !self.platform_collision(window)
+                elsif SF::Keyboard.key_pressed?(SF::Keyboard::Up) && !self.climbeable_collision(window)
+                    self.climb_climbeable(window)
+                else
+                    entity.move(SF.vector2(0, gravity))
+                end
             end
         end
 
         def self.can_jump?(window)
          if self.level_platform_array.all? { |platform| Sprites::Player.check_feet_collision(window, platform.sprite) == false }
-            self.gravitational_pull(Sprites::Player.retrieve_sprite, 1)
+            #self.gravitational_pull(Sprites::Player.retrieve_sprite, 1, window)
             return false
          else
             return true
          end
         end
-
         def self.platform_collision(window)
             if self.level_platform_array.all?{ |platform| Sprites::Player.check_feet_collision(window, platform.sprite) == false }
-               self.gravitational_pull(Sprites::Player.retrieve_sprite, 1)
+               #self.gravitational_pull(Sprites::Player.retrieve_sprite, 1)
+               return true
+            else 
+               return false
             end
         end
+        def self.climbeable_collision(window)
+            if self.level_climbeable_array.all?{ |climbeable| Sprites::Player.check_sprite_collision(window, climbeable.sprite) == false}
+            return true
+        else
+            return false
+        end
+        end
+
+        def self.climb_climbeable(window)
+            self.level_climbeable_array.each{ |climbeable| 
+        if Sprites::Player.check_sprite_collision(window, climbeable.sprite)
+            Sprites::Player.move_player_sprite(window, 0, -1)
+        end}
+        end
+
+
         def self.wall_collision(window)
             self.level_wall_array.each{|wall|
             if Sprites::Player.check_sprite_collision(window, wall.sprite)
@@ -89,6 +115,7 @@ module Maps
             end
           end
           
+
         def self.draw_elements(window)
             self.normal_gameplay_view(window)
 
@@ -146,8 +173,8 @@ module Maps
             self.level_teleport_array.each { |teleport|
             teleport.sprite.texture_rect = Animations::Portal.portal_animation
             window.draw(teleport.sprite)}
-            
-            self.platform_collision(window)
+            self.gravitational_pull(Sprites::Player.retrieve_sprite, gravity, window)
+            #self.platform_collision(window)
             self.wall_collision(window)
         end
      end
