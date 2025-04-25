9645484e39382bf9b02938db6f602a6eb6a69dd4
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-04-25 13:19:29 -0400

    changed whackeable sounds

--- src/sound/sfx.cr
+++ src/sound/sfx.cr
@@ -3,9 +3,16 @@ require "../sound/music.cr"
 
 module SFX
     class CombatSFX
-       # BB_GUN_SFX_01_BUFFER = SF::SoundBuffer.from_file("../sound/sfx/bb_gun_01.ogg")
+
         BB_GUN_SFX_01 = SF::Sound.new(SF::SoundBuffer.from_file("../sound/sfx/bb_gun_01.ogg"))
 
         SWORD_SWING_SFX_01 = SF::Sound.new(SF::SoundBuffer.from_file("../sound/sfx/sword_01.ogg"))
+    end
+
+    class WhackeableSFX
+
+        FABRIC_HIT_01 = SF::Sound.new(SF::SoundBuffer.from_file("../sound/sfx/fabric_hit_01.ogg"))
+
+        ROCK_HIT_01 = SF::Sound.new(SF::SoundBuffer.from_file("../sound/sfx/rock_hit_01.ogg"))
     end
 end
