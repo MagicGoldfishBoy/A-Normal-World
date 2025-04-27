9a01114f93cba1e5c55b2d5798f534a921fd575e
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-04-27 18:34:38 -0400

    added more hats

--- src/inventory/equipment/hat.cr
+++ src/inventory/equipment/hat.cr
@@ -27,10 +27,10 @@ module Hat
         @@black_baseball_cap = BaseballCap.new("Black Baseball Cap", "Bk/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "black")
         @@black_baseball_cap.sprite.color = SF.color(25, 25, 25) 
 
-        @@tan_baseball_cap = BaseballCap.new("Tan Baseball Cap", "T/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "tan")
+        @@tan_baseball_cap = BaseballCap.new("Tan Baseball Cap", "T/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "brown")
         @@tan_baseball_cap.sprite.color = SF.color(210, 180, 140)
 
-        @@brown_baseball_cap = BaseballCap.new("Brown Baseball Cap", "T/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "tan")
+        @@brown_baseball_cap = BaseballCap.new("Brown Baseball Cap", "T/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "brown")
         @@brown_baseball_cap.sprite.color = SF.color(139, 69, 19)
 
         @@red_baseball_cap = BaseballCap.new("Red Baseball Cap", "R/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "red")
@@ -41,5 +41,35 @@ module Hat
 
         @@yellow_baseball_cap = BaseballCap.new("Yellow Baseball Cap", "Y/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "yellow")
         @@yellow_baseball_cap.sprite.color = SF.color(228, 208, 10)
+
+        @@yellow_green_baseball_cap = BaseballCap.new("Yellow-Green Baseball Cap", "Y-Gn/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "green")
+        @@yellow_green_baseball_cap.sprite.color = SF.color(170, 255, 0)
+
+        @@green_baseball_cap = BaseballCap.new("Green Baseball Cap", "Gn/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "green")
+        @@green_baseball_cap.sprite.color = SF.color(34, 139, 34)
+
+        @@turquoise_baseball_cap = BaseballCap.new("Turquoise Baseball Cap", "Tq/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "blue")
+        @@turquoise_baseball_cap.sprite.color = SF.color(64, 224, 208)
+
+        @@light_blue_baseball_cap = BaseballCap.new("Light Blue Baseball Cap", "L-Bl/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "blue")
+        @@light_blue_baseball_cap.sprite.color = SF.color(137, 207, 240)
+
+        @@blue_baseball_cap = BaseballCap.new("Blue Baseball Cap", "Bl/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "blue")
+        @@blue_baseball_cap.sprite.color = SF.color(20, 52, 164)
+
+        @@indigo_baseball_cap = BaseballCap.new("Indigo Baseball Cap", "In/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "purple")
+        @@indigo_baseball_cap.sprite.color = SF.color(63, 0, 255)
+
+        @@light_purple_baseball_cap = BaseballCap.new("Light Purple Baseball Cap", "L-Pr/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "purple")
+        @@light_purple_baseball_cap.sprite.color = SF.color(191, 64, 191)
+
+        @@purple_baseball_cap = BaseballCap.new("Purple Baseball Cap", "Pr/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "purple")
+        @@purple_baseball_cap.sprite.color = SF.color(112, 41, 99)
+
+        @@pink_baseball_cap = BaseballCap.new("Pink Baseball Cap", "Pk/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "pink")
+        @@pink_baseball_cap.sprite.color = SF.color(255, 192, 203)
+
+        @@hot_pink_baseball_cap = BaseballCap.new("Hot-Pink Baseball Cap", "H-Pk/Baseball Cap", true, SF::Sprite.new(WHITE_BASEBALL_CAP_TEXTURE), "pink")
+        @@hot_pink_baseball_cap.sprite.color = SF.color(255, 105, 180)
     end
 end
