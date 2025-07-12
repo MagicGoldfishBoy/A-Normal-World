8b9acaf3b713ac6f1d7800fc52c536c529621397
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-07-12 09:38:09 -0400

    added clothing color constants for convenience

---
+++ graphics/color.cr
@@ -0,0 +1,22 @@
+require "crsfml"
+module Color
+    class ClothingColor
+        GREY = SF::Color.new(100, 100, 100)
+        BLACK = SF::Color.new(25, 25, 25)
+        TAN = SF::Color.new(210, 180, 140)
+        BROWN = SF::Color.new(139, 69, 19)
+        RED = SF::Color.new(136, 8, 8)
+        ORANGE = SF::Color.new(255, 117, 24)
+        YELLOW = SF::Color.new(228, 208, 10)
+        YELLOW_GREEN = SF::Color.new(170, 255, 0)
+        GREEN = SF::Color.new(34, 139, 34)
+        TURQUOISE = SF::Color.new(64, 224, 208)
+        LIGHT_BLUE = SF::Color.new(137, 207, 240)
+        BLUE = SF::Color.new(20, 52, 164)
+        INDIGO = SF::Color.new(63, 0, 255)
+        LIGHT_PURPLE = SF::Color.new(191, 64, 191)
+        PURPLE = SF::Color.new(112, 41, 99)
+        PINK = SF::Color.new(255, 192, 203)
+        HOT_PINK = SF::Color.new(255, 105, 180)
+    end
+end
