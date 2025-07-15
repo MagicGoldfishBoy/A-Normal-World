cdeb2066575ccc9c5168bb85c4eeae3447f2b774
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-07-15 16:17:52 -0400

    added sweatbands

--- src/inventory/equipment/hat.cr
+++ src/inventory/equipment/hat.cr
@@ -68,16 +68,17 @@ module Hat
      
     end
     class BaseballCap < HatBase
-        @@white_baseball_cap = BaseballCap.new("White Baseball Cap", "W/Baseball Cap", true, SF::Sprite.new(BASEBALL_CAP_TEXTURE), 
-        "white", true, DEFAULT_HAT_EQUIP_SFX)
+        def initialize(name : String, id : String, is_owned : Bool, sprite : SF::Sprite, color : String, covers_hair : Bool, sfx : SF::Sound)
+            super(name, id, is_owned, sprite, color, covers_hair = true, sfx)
+        end
 
-        @@grey_baseball_cap = BaseballCap.new("Grey Baseball Cap", "Gy/Baseball Cap", true, SF::Sprite.new(BASEBALL_CAP_TEXTURE), "grey", 
-        true, DEFAULT_HAT_EQUIP_SFX)
+        @@white_baseball_cap = BaseballCap.new("White Baseball Cap", "W/Baseball Cap", true, SF::Sprite.new(BASEBALL_CAP_TEXTURE), "white", true, DEFAULT_HAT_EQUIP_SFX)
+
+        @@grey_baseball_cap = BaseballCap.new("Grey Baseball Cap", "Gy/Baseball Cap", true, SF::Sprite.new(BASEBALL_CAP_TEXTURE), "grey", true, DEFAULT_HAT_EQUIP_SFX)
         @@grey_baseball_cap.sprite.as(SF::Sprite).color = Color::ClothingColor::GREY
 
-        @@black_baseball_cap = BaseballCap.new("Black Baseball Cap", "Bk/Baseball Cap", true, SF::Sprite.new(BASEBALL_CAP_TEXTURE), "black", 
-        true, DEFAULT_HAT_EQUIP_SFX)
-        @@black_baseball_cap.sprite.as(SF::Sprite).color = Color::ClothingColor::BLACK 
+        @@black_baseball_cap = BaseballCap.new("Black Baseball Cap", "Bk/Baseball Cap", true, SF::Sprite.new(BASEBALL_CAP_TEXTURE), "black", true, DEFAULT_HAT_EQUIP_SFX)
+        @@black_baseball_cap.sprite.as(SF::Sprite).color = Color::ClothingColor::BLACK
 
         @@tan_baseball_cap = BaseballCap.new("Tan Baseball Cap", "T/Baseball Cap", true, SF::Sprite.new(BASEBALL_CAP_TEXTURE), "tan", true, DEFAULT_HAT_EQUIP_SFX)
         @@tan_baseball_cap.sprite.as(SF::Sprite).color = Color::ClothingColor::TAN
@@ -125,6 +126,9 @@ module Hat
         @@hot_pink_baseball_cap.sprite.as(SF::Sprite).color = Color::ClothingColor::HOT_PINK
     end
     class Beanie < HatBase
+        def initialize(name : String, id : String, is_owned : Bool, sprite : SF::Sprite, color : String, covers_hair : Bool, sfx : SF::Sound)
+            super(name, id, is_owned, sprite, color, covers_hair = true, sfx)
+        end
         @@white_beanie = Beanie.new("White Beanie", "W/Beanie", true, SF::Sprite.new(BEANIE_TEXTURE), "white", true, DEFAULT_HAT_EQUIP_SFX)
 
         @@grey_beanie = Beanie.new("Grey Beanie", "Gy/Beanie", true, SF::Sprite.new(BEANIE_TEXTURE), "grey", true, DEFAULT_HAT_EQUIP_SFX)
@@ -177,5 +181,62 @@ module Hat
 
         @@hot_pink_beanie = Beanie.new("Hot-Pink Beanie", "H-Pk/Beanie", false, SF::Sprite.new(BEANIE_TEXTURE), "hot-pink", true, DEFAULT_HAT_EQUIP_SFX)
         @@hot_pink_beanie.sprite.as(SF::Sprite).color = Color::ClothingColor::HOT_PINK
+    end
+    class SweatBand < HatBase
+        def initialize(name : String, id : String, is_owned : Bool, sprite : SF::Sprite, color : String, covers_hair : Bool, sfx : SF::Sound)
+            super(name, id, is_owned, sprite, color, covers_hair = false, sfx)
+        end
+        @@white_sweatband = SweatBand.new("White Sweatband", "W/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "white", false, DEFAULT_HAT_EQUIP_SFX)
+
+        @@grey_sweatband = SweatBand.new("Grey Sweatband", "Gy/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "grey", true, DEFAULT_HAT_EQUIP_SFX)
+        @@grey_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::GREY
+
+        @@black_sweatband = SweatBand.new("Black Sweatband", "Bk/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "black", true, DEFAULT_HAT_EQUIP_SFX)
+        @@black_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::BLACK
+
+        @@tan_sweatband = SweatBand.new("Tan Sweatband", "T/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "tan", true, DEFAULT_HAT_EQUIP_SFX)
+        @@tan_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::TAN
+
+        @@brown_sweatband = SweatBand.new("Brown Sweatband", "Br/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "brown", true, DEFAULT_HAT_EQUIP_SFX)
+        @@brown_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::BROWN
+
+        @@red_sweatband = SweatBand.new("Red Sweatband", "R/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "red", true, DEFAULT_HAT_EQUIP_SFX)
+        @@red_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::RED
+
+        @@orange_sweatband = SweatBand.new("Orange Sweatband", "O/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "orange", true, DEFAULT_HAT_EQUIP_SFX)
+        @@orange_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::ORANGE
+
+        @@yellow_sweatband = SweatBand.new("Yellow Sweatband", "Y/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "yellow", true, DEFAULT_HAT_EQUIP_SFX)
+        @@yellow_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::YELLOW
+
+        @@yellow_green_sweatband = SweatBand.new("Yellow-Green Sweatband", "Y-Gn/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "yellow-green", true, DEFAULT_HAT_EQUIP_SFX)
+        @@yellow_green_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::YELLOW_GREEN
+
+        @@green_sweatband = SweatBand.new("Green Sweatband", "Gn/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "green", true, DEFAULT_HAT_EQUIP_SFX)
+        @@green_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::GREEN
+
+        @@turquoise_sweatband = SweatBand.new("Turquoise Sweatband", "Tq/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "turquoise", true, DEFAULT_HAT_EQUIP_SFX)
+        @@turquoise_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::TURQUOISE
+
+        @@light_blue_sweatband = SweatBand.new("Light Blue Sweatband", "L-Bl/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "light-blue", true, DEFAULT_HAT_EQUIP_SFX)
+        @@light_blue_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::LIGHT_BLUE
+
+        @@blue_sweatband = SweatBand.new("Blue Sweatband", "Bl/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "blue", true, DEFAULT_HAT_EQUIP_SFX)
+        @@blue_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::BLUE
+
+        @@indigo_sweatband = SweatBand.new("Indigo Sweatband", "In/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "indigo", true, DEFAULT_HAT_EQUIP_SFX)
+        @@indigo_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::INDIGO
+
+        @@light_purple_sweatband = SweatBand.new("Light Purple Sweatband", "L-Pr/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "light-purple", true, DEFAULT_HAT_EQUIP_SFX)
+        @@light_purple_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::LIGHT_PURPLE
+
+        @@purple_sweatband = SweatBand.new("Purple Sweatband", "Pr/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "purple", true, DEFAULT_HAT_EQUIP_SFX)
+        @@purple_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::PURPLE
+
+        @@pink_sweatband = SweatBand.new("Pink Sweatband", "Pk/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "pink", true, DEFAULT_HAT_EQUIP_SFX)
+        @@pink_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::PINK
+
+        @@hot_pink_sweatband = SweatBand.new("Hot-Pink Sweatband", "H-Pk/Sweatband", false, SF::Sprite.new(WHITE_SWEATBAND_TEXTURE), "hot-pink", true, DEFAULT_HAT_EQUIP_SFX)
+        @@hot_pink_sweatband.sprite.as(SF::Sprite).color = Color::ClothingColor::HOT_PINK
     end
 end
