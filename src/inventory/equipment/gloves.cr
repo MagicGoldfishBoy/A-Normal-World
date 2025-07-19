2e3f880f4c4f65a5530f351673ad41e0e68d779c
MagicGoldfishBoy <magicgoldfishboy@gmail.com>
2025-07-19 12:10:40 -0400

    added fingerless gloves

--- src/inventory/equipment/gloves.cr
+++ src/inventory/equipment/gloves.cr
@@ -121,5 +121,60 @@ module Gloves
             @@hot_pink_gloves = ShortGloves.new("Hot Pink Gloves", "short_gloves_hot_pink", false, SF::Sprite.new(GLOVES_WHITE_TEXTURE), "hot-pink", DEFAULT_GLOVES_EQUIP_SFX)
             @@hot_pink_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::HOT_PINK
         end
+
+        class FingerlessGloves < GlovesBase
+            @@white_fingerless_gloves = FingerlessGloves.new("White Fingerless Gloves", "fingerless_gloves_white", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "white", DEFAULT_GLOVES_EQUIP_SFX)
+
+            @@grey_fingerless_gloves = FingerlessGloves.new("Grey Fingerless Gloves", "fingerless_gloves_grey", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "grey", DEFAULT_GLOVES_EQUIP_SFX)
+            @@grey_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::GREY
+
+            @@black_fingerless_gloves = FingerlessGloves.new("Black Fingerless Gloves", "fingerless_gloves_black", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "black", DEFAULT_GLOVES_EQUIP_SFX)
+            @@black_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::BLACK
+
+            @@tan_fingerless_gloves = FingerlessGloves.new("Tan Fingerless Gloves", "fingerless_gloves_tan", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "tan", DEFAULT_GLOVES_EQUIP_SFX)
+            @@tan_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::TAN
+
+            @@brown_fingerless_gloves = FingerlessGloves.new("Brown Fingerless Gloves", "fingerless_gloves_brown", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "brown", DEFAULT_GLOVES_EQUIP_SFX)
+            @@brown_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::BROWN
+
+            @@red_fingerless_gloves = FingerlessGloves.new("Red Fingerless Gloves", "fingerless_gloves_red", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "red", DEFAULT_GLOVES_EQUIP_SFX)
+            @@red_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::RED
+
+            @@orange_fingerless_gloves = FingerlessGloves.new("Orange Fingerless Gloves", "fingerless_gloves_orange", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "orange", DEFAULT_GLOVES_EQUIP_SFX)
+            @@orange_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::ORANGE
+
+            @@yellow_fingerless_gloves = FingerlessGloves.new("Yellow Fingerless Gloves", "fingerless_gloves_yellow", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "yellow", DEFAULT_GLOVES_EQUIP_SFX)
+            @@yellow_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::YELLOW
+
+            @@yellow_green_fingerless_gloves = FingerlessGloves.new("Yellow Green Fingerless Gloves", "fingerless_gloves_yellow_green", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "yellow-green", DEFAULT_GLOVES_EQUIP_SFX)
+            @@yellow_green_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::YELLOW_GREEN
+
+            @@green_fingerless_gloves = FingerlessGloves.new("Green Fingerless Gloves", "fingerless_gloves_green", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "green", DEFAULT_GLOVES_EQUIP_SFX)
+            @@green_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::GREEN
+
+            @@turquoise_fingerless_gloves = FingerlessGloves.new("Turquoise Fingerless Gloves", "fingerless_gloves_turquoise", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "turquoise", DEFAULT_GLOVES_EQUIP_SFX)
+            @@turquoise_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::TURQUOISE
+
+            @@light_blue_fingerless_gloves = FingerlessGloves.new("Light Blue Fingerless Gloves", "fingerless_gloves_light_blue", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "light-blue", DEFAULT_GLOVES_EQUIP_SFX)
+            @@light_blue_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::LIGHT_BLUE
+
+            @@blue_fingerless_gloves = FingerlessGloves.new("Blue Fingerless Gloves", "fingerless_gloves_blue", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "blue", DEFAULT_GLOVES_EQUIP_SFX)
+            @@blue_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::BLUE
+
+            @@indigo_fingerless_gloves = FingerlessGloves.new("Indigo Fingerless Gloves", "fingerless_gloves_indigo", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "indigo", DEFAULT_GLOVES_EQUIP_SFX)
+            @@indigo_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::INDIGO
+
+            @@light_purple_fingerless_gloves = FingerlessGloves.new("Light Purple Fingerless Gloves", "fingerless_gloves_light_purple", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "light-purple", DEFAULT_GLOVES_EQUIP_SFX)
+            @@light_purple_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::LIGHT_PURPLE
+
+            @@purple_fingerless_gloves = FingerlessGloves.new("Purple Fingerless Gloves", "fingerless_gloves_purple", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "purple", DEFAULT_GLOVES_EQUIP_SFX)
+            @@purple_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::PURPLE
+
+            @@pink_fingerless_gloves = FingerlessGloves.new("Pink Fingerless Gloves", "fingerless_gloves_pink", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "pink", DEFAULT_GLOVES_EQUIP_SFX)
+            @@pink_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::PINK
+
+            @@hot_pink_fingerless_gloves = FingerlessGloves.new("Hot Pink Fingerless Gloves", "fingerless_gloves_hot_pink", false, SF::Sprite.new(FINGERLESS_GLOVES_WHITE_TEXTURE), "hot-pink", DEFAULT_GLOVES_EQUIP_SFX)
+            @@hot_pink_fingerless_gloves.sprite.as(SF::Sprite).color = Color::ClothingColor::HOT_PINK
+        end
     end
 end
